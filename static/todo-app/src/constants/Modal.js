6c86dfd45bd332cc69aebbc3e9f4a87f591dbe5a
olegkravchuk <oleg-1995@meta.ua>
2017-02-20 13:20:59 +0200

    rename

---
+++ static/todo-app/src/constants/Modal.js
@@ -0,0 +1,25 @@
+export const SHOW_MODAL = 'SHOW_MODAL';
+export const HIDE_MODAL = 'HIDE_MODAL';
+
+export const customStyles = {
+    overlay : {
+        position          : 'fixed',
+        top               : 0,
+        left              : 0,
+        right             : 0,
+        bottom            : 0,
+        backgroundColor   : 'rgba(255, 255, 255, 0.75)',
+        zIndex: 1002
+    },
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)',
+        height: '65%'
+        //width: '50% '
+
+    }
+};
